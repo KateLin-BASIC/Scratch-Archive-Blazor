d416a9145ed6da515ae43fab36507c636aae5b9b
Kate Lin <63230494+KateLin-BASIC@users.noreply.github.com>
2021-11-16 20:57:26 +0900

    초기 커밋.

---
+++ Scratch Archive/Models/ScratchVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scratch_Archive.Models
+{
+    public class ScratchVersion
+    {
+        public byte Id { get; set; }
+        public string? Name { get; set; }
+        public string? FileUrl { get; set; }
+    }
+}
---
+++ Scratch Archive/wwwroot/assets/js/app.js
@@ -0,0 +1,17 @@
+﻿function resetBackgroundColor() {
+    document.body.style.background = "white";
+}
+
+function resizeCanvas() {
+    var canvas = document.getElementById("canvas");
+
+    canvas.style.width = window.innerWidth + "px";
+    setTimeout(function () {
+        canvas.style.height = window.innerHeight + "px";
+    }, 0);
+};
+
+function registerEvent() {
+    window.onresize = resizeCanvas;
+    resizeCanvas();
+};
